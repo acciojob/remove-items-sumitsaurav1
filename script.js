ee9f97ed6a392833d12117a1510909b951699509
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-06-18 08:38:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let colorSelect = document.getElementById("colorSelect");
+let button = document.getElementById("btn");
+
+button.addEventListener('click',(e)=>{
+    e.preventDefault();
+
+    let colors = document.querySelectorAll("#colorSelect option")
+  
+    for(let i=0;i<colors.length;i++){
+
+      if(colorSelect.value===colors[i].value){
+        colorSelect.removeChild(colors[i]);
+      }
+    }
+})
